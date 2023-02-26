9b4cb109319e29abfba51ee86aa165b1da089906
nahakritesh <riteshkumarfsds@gmail.com>
2023-02-26 20:13:20 +0530

    data transformation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys,os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
-# from sensor.components.data_transformation import DataTransformation
+from sensor.components.data_transformation import DataTransformation
 # from sensor.components.model_trainer import ModelTrainer
 # from sensor.components.model_evaluation import ModelEvaluation
 # from sensor.components.model_pusher import ModelPusher
@@ -23,3 +23,8 @@ data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
 data_validation_config=config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
 data_validation=DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
 data_validation_artifact=data_validation.initiate_data_validation()
+
+# Data Transformation
+data_transformation_config=config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+data_transformation=DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
+data_transformation_artifact=data_transformation.initiate_data_transformation()
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -6,6 +6,9 @@ from sensor.logger import logging
 FILE_NAME='sensor.csv'
 TRAIN_FILE_NAME='train.csv'
 TEST_FILE_NAME='test.csv'
+TRANSFORMER_OBJECT_FILE_NAME='tranformer.pkl'
+TARGET_ENCODER_OBJECT_FILE_NAME='target_encoder.pkl'
+MODEL_FILE_NAME='model.pkl'
 
 class TrainingPipelineConfig:
     def __init__(self):
@@ -42,3 +45,11 @@ class DataValidationConfig:
             self.base_file_path=os.path.join('aps_failure_training_set1.csv')
         except Exception as e:
             raise SensorException(e, sys)
+
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir = os.path.join(training_pipeline_config.artifacts_dir , "data_transformation")
+        self.transform_object_path = os.path.join(self.data_transformation_dir,"transformer",TRANSFORMER_OBJECT_FILE_NAME)
+        self.transformed_train_path =  os.path.join(self.data_transformation_dir,"transformed",TRAIN_FILE_NAME.replace("csv","npz"))
+        self.transformed_test_path =os.path.join(self.data_transformation_dir,"transformed",TEST_FILE_NAME.replace("csv","npz"))
+        self.target_encoder_path = os.path.join(self.data_transformation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
